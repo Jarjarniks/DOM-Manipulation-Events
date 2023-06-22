3c7958a0d690668a0475bbd954d311e552865088
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-22 13:32:12 +0200

    Add p tag & set class toggle

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
+const p = document.createElement('p');
+p.classList.toggle('red');
+p.textContent = "Hey I'm red!"
+
 container.appendChild(content);
