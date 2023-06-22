d85957a41a3cd4285a09ac9d43496159343cbb12
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-22 14:14:33 +0200

    Method 2 event code added

--- app.js
+++ app.js
@@ -32,3 +32,8 @@ containerDiv.appendChild(h1);
 containerDiv.appendChild(p2);
 
 /* Events */
+// method 2, better, but a dom element can only have 1 "onclick" property, meaning we can't run multiple functions.
+const btn = document.querySelector('#btn');
+btn.onclick = () => {
+    alert('Hello world');
+}
